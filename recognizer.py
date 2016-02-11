5460051c9852fa24bdc09381e29be96aecb14e10
Toshiba Laptop <sarathm00@gmail.com>
2016-02-11 21:52:41 +0530

    Working

--- recognizer.py
+++ recognizer.py
@@ -1,10 +1,7 @@
 import pyaudio
 import wave
-import sphinxbase
-from os import environ, path
+import os
 
-from pocketsphinx.pocketsphinx import *
-from sphinxbase.sphinxbase import *
 # Import sometimes fails first time around because of a Cython issue.
 try:
     import pocketsphinx
@@ -13,54 +10,30 @@ except ValueError:
 
 # Paths
 BASE_PATH = '/home/sm/Documents/scribe'
-HMDIR = path.join(BASE_PATH, "hmm")
-LMDIR = path.join(BASE_PATH, "lm/cmusphinx-5.0-en-us.lm.dmp")
-DICTD = path.join(BASE_PATH, "dict/cmu07a.dic")
+HMDIR = os.path.join(BASE_PATH, "hmm")
+LMDIR = os.path.join(BASE_PATH, "lm/cmusphinx-5.0-en-us.lm.dmp")
+DICTD = os.path.join(BASE_PATH, "dict/cmu07a.dic")
 
 # Options
-CHUNK = 128 # The size of each audio chunk coming from the input device.
 FORMAT = pyaudio.paInt16 # Should not be changed, as this format is best for speech recognition.
+CHANNELS = 1
 RATE = 16000 # Speech recognition only works well with this rate.  Don't change unless your microphone demands it.
-RECORD_SECONDS = 5 # Number of seconds to record, can be changed.
+CHUNK = 128 # The size of each audio chunk coming from the input device.
+RECORD_SECONDS = 2 # Number of seconds to record, can be changed.
 WAVE_OUTPUT_FILENAME = "output.wav" # Where to save the recording from the microphone.
-
-def find_device(p, tags):
-    """
-    Find an audio device to read input from.
-    """
-    device_index = None
-    for i in range(p.get_device_count()):
-        devinfo = p.get_device_info_by_index(i)
-        print("Device %d: %s" % (i, devinfo["name"]))
-
-        for keyword in tags:
-            if keyword in devinfo["name"].lower():
-                print("Found an input: device %d - %s"%(i, devinfo["name"]))
-                device_index = i
-                return device_index
-
-    if device_index is None:
-        print("No preferred input found; using default input device.")
-
-    return device_index
 
 def save_audio(wav_file):
     """
     Stream audio from an input device and save it.
     """
     p = pyaudio.PyAudio()
-
-    device = find_device(p, ["default", "analg", "audio"])
-    device_info = p.get_device_info_by_index(device)
-    channels = int(device_info['maxInputChannels'])
-
     stream = p.open(
         format=FORMAT,
-        channels=channels,
+        channels=CHANNELS,
         rate=RATE,
         input=True,
         frames_per_buffer=CHUNK,
-        input_device_index=device
+        #input_device_index=device
     )
 
     print("* recording")
@@ -79,7 +52,7 @@ def save_audio(wav_file):
     p.terminate()
 
     wf = wave.open(wav_file, 'wb')
-    wf.setnchannels(channels)
+    wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
     wf.setframerate(RATE)
     wf.writeframes(b''.join(frames))
@@ -90,14 +63,14 @@ def recognize(wav_file):
     Run speech recognition on a given file.
     """
     # Create a decoder with certain model
-    config = Decoder.default_config()
+    config = pocketsphinx.Decoder.default_config()
     config.set_string('-hmm', HMDIR)
     config.set_string('-lm', LMDIR)
     config.set_string('-dict', DICTD)
     config.set_string('-logfn', '/dev/null')
 
     # Decode streaming data.
-    speech_rec = Decoder(config)
+    speech_rec = pocketsphinx.Decoder(config)
     speech_rec.start_utt()
     stream = open(wav_file, 'rb')
     while True:
@@ -123,5 +96,4 @@ def recognize(wav_file):
 # Run the thing!
 if __name__ == '__main__':
     save_audio(WAVE_OUTPUT_FILENAME)
-    result = recognize('../Felix/3.wav')
-    #print "You just said: {0}".format(result[0])
+    result = recognize(WAVE_OUTPUT_FILENAME)
